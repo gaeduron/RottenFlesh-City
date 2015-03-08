b49e0b6b5c17966c88468c03aac7b204d152bc67
gaetan duron <gaetan.duron@gmail.com>
2015-03-08 23:08:02 +0100

    Installation de pygame
    J’ai enfin fini  apres une semaine d’éffort a l’installer !! Fiouu…
    Ce code sert à tester pygame en faisant  apparaite une fenetre

---
+++ pygame test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 19:23:43 2015
+
+@author: gaetanduron
+"""
+
+import pygame
+
+if ( __name__ == "__main__" ):
+    pygame.init()
+    
+    window_size = window_width, window_height = 640, 480
+    window = pygame.display.set_mode(window_size)
+    
+    running = True
+    
+    while ( running ):
+        for event in pygame.event.get():
+            if ( event.type == pygame.QUIT):
+                running = False
+                
+    pygame.quit()
