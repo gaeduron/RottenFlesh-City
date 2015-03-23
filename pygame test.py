2c4a7e6d6bbe714880cdadce2ef29166b88ea507
Stan <staburillard@users.noreply.github.com>
2015-03-23 12:24:59 +0100

    Update pygame test.py
    modif pep8

--- pygame test.py
+++ pygame test.py
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Mar  8 19:23:43 2015
-
-@author: gaetanduron
-"""
 
 import pygame
 
-if ( __name__ == "__main__" ):
+if (__name__ == "__main__"):
     pygame.init()
-    
+
     window_size = window_width, window_height = 640, 480
     window = pygame.display.set_mode(window_size)
-    
+
     running = True
-    
-    while ( running ):
+
+    while (running):
         for event in pygame.event.get():
-            if ( event.type == pygame.QUIT):
+            if (event.type == pygame.QUIT):
                 running = False
-                
-    pygame.quit()
+
+pygame.quit()
